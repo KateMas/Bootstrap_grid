0e2520c08c79f830db2f1ea24b54d99a387199d2
Ekaterina <e.masyuta@gmail.com>
2015-03-22 10:18:53 +0200

    link main&sidebar

--- client/styles/config.rb
+++ client/styles/config.rb
@@ -1,4 +1,6 @@
 # Require any additional compass plugins here.
+require 'compass/import-once/activate'
+require "bootstrap-sass"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -23,4 +25,3 @@ output_style = :expanded
 preferred_syntax = :scss
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-#require 'bootstrap-sass'
